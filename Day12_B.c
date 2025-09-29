1382244f3e9091efafc9b8153c28bf8a777264f3
bhatnagarmedha28-ctrl <bhatnagarmedha28@gmail.com>
2025-09-30 01:51:47 +0530

    // Write a program to calculate electricity bill based on units consumed

---
+++ Day12_B.c
@@ -0,0 +1,23 @@
+//Q24: Write a program to calculate electricity bill based on units consumed 
+
+#include <stdio.h>
+int main() {
+    int units;
+    float bill = 0.0;
+
+    printf("Enter the number of units consumed: ");
+    scanf("%d", &units);
+
+    if (units <= 100) {
+        bill = units * 5.0;
+    } else if (units <= 200) {
+        bill = (100 * 5.0) + ((units - 100) * 7.0);
+    } else if (units <= 300) {
+        bill = (100 * 5.0) + (100 * 7.0) + ((units - 200) * 10.0);
+    } else {
+        bill = (100 * 5.0) + (100 * 7.0) + (100 * 10.0) + ((units - 300) * 12.0);
+    }
+
+    printf("Total electricity bill: ₹%.2f\n", bill);
+    return 0;
+}
