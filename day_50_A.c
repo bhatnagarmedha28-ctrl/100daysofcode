0fc2442029326dc72165042cb0a8b9c059a4779f
bhatnagarmedha28-ctrl <bhatnagarmedha28@gmail.com>
2025-11-03 16:12:15 +0530

    //Change the date format from dd/04/yyyy to dd-Apr-yyyy

---
+++ day_50_A.c
@@ -0,0 +1,10 @@
+//Change the date format from dd/04/yyyy to dd-Apr-yyyy
+
+#include <stdio.h>
+
+int main() {
+    char date[11];
+    scanf("%s", date);
+    printf("%c%c-Apr-%c%c%c%c", date[0], date[1], date[6], date[7], date[8], date[9]);
+    return 0;
+}
